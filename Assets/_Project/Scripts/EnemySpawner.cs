0f2608efe64daf5aa586e6d08cd0b74e9ca539da
Artem K <xxkot1990@gmail.com>
2026-01-14 19:53:19 +0200

    Revert "Мейн"
    This reverts commit 5e5fc822e7ff5c7375c47363ffffeaa77138b0c0.

--- Assets/_Project/Scripts/EnemySpawner.cs
+++ Assets/_Project/Scripts/EnemySpawner.cs
@@ -1,8 +1,5 @@
-#pragma warning disable
 using UnityEngine;
-using PrimeTween;
 
-<<<<<<< Updated upstream
 public class EnemySpawner : MonoBehaviour
 {
     [Header("References")]
@@ -71,55 +68,3 @@ public class EnemySpawner : MonoBehaviour
         return GameObject.FindGameObjectsWithTag("Enemy").Length;
     }
 }
-=======
-public class EnemySpawner : MonoBehaviour {	
-	[Header("Spawn settings")]
-	public GameObject enemyPrefab;
-	public Transform player;
-	public float spawnInterval = 1.5F;
-	public float minDistanceFromPlayer = 5F;
-	public int maxAttempts = 20;
-	[Header("Map size")]
-	public float mapWidth = 50F;
-	public float mapHeight = 50F;
-	private float timer;
-	
-	private void Update() {
-		timer += Time.deltaTime;
-		if((timer >= spawnInterval)) {
-			timer = 0F;
-			SpawnEnemy();
-		}
-	}
-	
-	private void SpawnEnemy() {
-		var spawnPosition = Vector3.zero;
-		var foundPosition = false;
-		for(int i = 0; i < maxAttempts; i += 1) {
-			var x = Random.Range((-mapWidth / 2F), (mapWidth / 2F));
-			var z = Random.Range((-mapHeight / 2F), (mapHeight / 2F));
-			spawnPosition = (this.transform.position + new Vector3(x, 0F, z));
-			var distanceToPlayer = Vector3.Distance(new Vector3(player.position.x, 0F, player.position.z), new Vector3(spawnPosition.x, 0F, spawnPosition.z));
-			if((distanceToPlayer >= minDistanceFromPlayer)) {
-				foundPosition = true;
-				break;
-			}
-		}
-		if(foundPosition) {
-			Tween.Scale(Object.Instantiate<GameObject>(enemyPrefab, spawnPosition, Quaternion.identity).transform, Vector3.zero, Vector3.one, 0.5F, Easing.Standard(Ease.OutBounce), 1, default(CycleMode), default(float), default(float), default(bool));
-		}
-	}
-	
-	private void OnDrawGizmosSelected() {
-		//Границы карты
-		Gizmos.color = Color.green;
-		Gizmos.DrawWireCube(this.transform.position, new Vector3(mapWidth, 0.1F, mapHeight));
-		//Минимальная дистанция от игрока
-		if((player != null)) {
-			Gizmos.color = Color.red;
-			Gizmos.DrawWireSphere(player.position, minDistanceFromPlayer);
-		}
-	}
-}
-
->>>>>>> Stashed changes
