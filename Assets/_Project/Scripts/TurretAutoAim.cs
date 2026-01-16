2d0cf3eb06c2187cb5c008eb284b01147a3f0117
Artem K <xxkot1990@gmail.com>
2026-01-16 11:21:04 +0200

    Обнова

--- Assets/_Project/Scripts/TurretAutoAim.cs
+++ Assets/_Project/Scripts/TurretAutoAim.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TurretAutoAim : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float attackRadius = 10f;
+    public float attackSpeed = 1f;
+    public float damage = 10f;
+    public float critChance = 0.1f;
 
-    // Update is called once per frame
+    string enemyTag = "Enemy";
+    Transform target;
+
     void Update()
     {
-        
+        target = null;
+        var enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float d = Mathf.Infinity;
+
+        foreach (var e in enemies)
+        {
+            float nd = Vector3.Distance(transform.position, e.transform.position);
+            if (nd < d && nd <= attackRadius)
+            {
+                d = nd;
+                target = e.transform;
+            }
+        }
+
+        if (!target) return;
+
+        Vector3 dir = target.position - transform.position;
+        dir.y = 0;
+        var rot = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, 1 * Time.deltaTime * 100f);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
     }
 }
